c269a8bee24ed2943dc6a78153247e06c3aed532
PatoPR <133245531+PatoPR@users.noreply.github.com>
2023-06-27 12:52:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-// Add these new JavaScript functions at the end of the file
+// Featured items carousel
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
 
-function navigateFeaturedItems(direction) {
-  const items = Array.from(document.getElementsByClassName('featured-item'));
-  const activeItem = document.querySelector('.featured-item.active');
-  let index = items.indexOf(activeItem);
-  if (direction === 'prev') {
-    index = (index - 1 + items.length) % items.length;
-  } else if (direction === 'next') {
-    index = (index + 1) % items.length;
+// Function to show the current slide
+const showSlide = (index) => {
+  // Hide all slides
+  slides.forEach((slide) => {
+    slide.style.display = 'none';
+  });
+
+  // Show the current slide
+  slides[index].style.display = 'block';
+};
+
+// Function to navigate to the previous slide
+const prevSlide = () => {
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
   }
-  activeItem.classList.remove('active');
-  items[index].classList.add('active');
-}
+  showSlide(currentSlide);
+};
 
-document.querySelector('.featured-item-prev').addEventListener('click', () => {
-  navigateFeaturedItems('prev');
-});
+// Function to navigate to the next slide
+const nextSlide = () => {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  showSlide(currentSlide);
+};
 
-document.querySelector('.featured-item-next').addEventListener('click', () => {
-  navigateFeaturedItems('next');
-});
+// Event listeners for navigation buttons
+prevButton.addEventListener('click', prevSlide);
+nextButton.addEventListener('click', nextSlide);
+
+// Automatic sliding every 5 seconds
+setInterval(nextSlide, 5000);
