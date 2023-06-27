20e288cdd5755baa6e697f0e2e085add04a0c112
PatoPR <133245531+PatoPR@users.noreply.github.com>
2023-06-27 12:01:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
+// Featured items carousel
 let currentSlide = 0;
-const slides = document.querySelectorAll(".slide");
+const slides = document.querySelectorAll('.slide');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
 
-function changeSlide(n) {
-  currentSlide += n;
+// Function to show the current slide
+const showSlide = (index) => {
+  // Hide all slides
+  slides.forEach((slide) => {
+    slide.style.display = 'none';
+  });
 
+  // Show the current slide
+  slides[index].style.display = 'block';
+};
+
+// Function to navigate to the previous slide
+const prevSlide = () => {
+  currentSlide--;
   if (currentSlide < 0) {
     currentSlide = slides.length - 1;
-  } else if (currentSlide >= slides.length) {
+  }
+  showSlide(currentSlide);
+};
+
+// Function to navigate to the next slide
+const nextSlide = () => {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
     currentSlide = 0;
   }
+  showSlide(currentSlide);
+};
 
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+// Event listeners for navigation buttons
+prevButton.addEventListener('click', prevSlide);
+nextButton.addEventListener('click', nextSlide);
 
-  slides[currentSlide].style.display = "block";
-}
+// Automatic sliding every 5 seconds
+setInterval(nextSlide, 5000);
 
-changeSlide(0); // Show the first slide by default
