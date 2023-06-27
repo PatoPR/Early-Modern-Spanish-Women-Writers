4b34eba33a7d6df5616d8b7c566f59da0bc4814b
PatoPR <133245531+PatoPR@users.noreply.github.com>
2023-06-27 12:38:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 // Add these new JavaScript functions at the end of the file
 
 function navigateFeaturedItems(direction) {
-    const items = Array.from(document.getElementsByClassName('featured-item'));
-    const activeItem = document.querySelector('.featured-item.active');
-    const activeIndex = items.indexOf(activeItem);
+  const items = Array.from(document.getElementsByClassName('featured-item'));
+  const activeItem = document.querySelector('.featured-item.active');
+  const activeIndex = items.indexOf(activeItem);
 
-    let newIndex;
-    if (direction === 'prev') {
-        newIndex = (activeIndex - 1 + items.length) % items.length;
-    } else if (direction === 'next') {
-        newIndex = (activeIndex + 1) % items.length;
-    }
+  let newIndex;
+  if (direction === 'prev') {
+    newIndex = (activeIndex - 1 + items.length) % items.length;
+  } else if (direction === 'next') {
+    newIndex = (activeIndex + 1) % items.length;
+  }
 
-    activeItem.classList.remove('active');
-    items[newIndex].classList.add('active');
+  activeItem.classList.remove('active');
+  items[newIndex].classList.add('active');
 }
 
 function setupFeaturedItemsSlider() {
-    const prevBtn = document.querySelector('.featured-item-prev');
-    const nextBtn = document.querySelector('.featured-item-next');
+  const prevBtn = document.querySelector('.featured-item-prev');
+  const nextBtn = document.querySelector('.featured-item-next');
 
-    prevBtn.addEventListener('click', () => navigateFeaturedItems('prev'));
-    nextBtn.addEventListener('click', () => navigateFeaturedItems('next'));
+  prevBtn.addEventListener('click', () => navigateFeaturedItems('prev'));
+  nextBtn.addEventListener('click', () => navigateFeaturedItems('next'));
 
-    setInterval(() => {
-        navigateFeaturedItems('next');
-    }, 5000);
+  setInterval(() => {
+    navigateFeaturedItems('next');
+  }, 5000);
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    setupFeaturedItemsSlider();
+  setupFeaturedItemsSlider();
 });
