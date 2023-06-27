cb1b508df875b47232406c3c176d457a99684284
PatoPR <133245531+PatoPR@users.noreply.github.com>
2023-06-27 12:45:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,31 +3,20 @@
 function navigateFeaturedItems(direction) {
   const items = Array.from(document.getElementsByClassName('featured-item'));
   const activeItem = document.querySelector('.featured-item.active');
-  const activeIndex = items.indexOf(activeItem);
-
-  let newIndex;
+  let index = items.indexOf(activeItem);
   if (direction === 'prev') {
-    newIndex = (activeIndex - 1 + items.length) % items.length;
+    index = (index - 1 + items.length) % items.length;
   } else if (direction === 'next') {
-    newIndex = (activeIndex + 1) % items.length;
+    index = (index + 1) % items.length;
   }
-
   activeItem.classList.remove('active');
-  items[newIndex].classList.add('active');
+  items[index].classList.add('active');
 }
 
-function setupFeaturedItemsSlider() {
-  const prevBtn = document.querySelector('.featured-item-prev');
-  const nextBtn = document.querySelector('.featured-item-next');
+document.querySelector('.featured-item-prev').addEventListener('click', () => {
+  navigateFeaturedItems('prev');
+});
 
-  prevBtn.addEventListener('click', () => navigateFeaturedItems('prev'));
-  nextBtn.addEventListener('click', () => navigateFeaturedItems('next'));
-
-  setInterval(() => {
-    navigateFeaturedItems('next');
-  }, 5000);
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-  setupFeaturedItemsSlider();
+document.querySelector('.featured-item-next').addEventListener('click', () => {
+  navigateFeaturedItems('next');
 });
